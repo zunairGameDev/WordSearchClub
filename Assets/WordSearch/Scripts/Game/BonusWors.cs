b1b240fb9d983d9e7d3a6fb778dcb98d7ce4da9c
Ali Jamshaid <58130030+UmairGameDev@users.noreply.github.com>
2024-10-30 11:58:01 +0500

    Delay added in bonus animation

--- Assets/WordSearch/Scripts/Game/BonusWors.cs
+++ Assets/WordSearch/Scripts/Game/BonusWors.cs
@@ -27,27 +27,27 @@ public class BonusWors : MonoBehaviour
         claimButton.SetActive(true);
         Sequence scaleSequence = DOTween.Sequence();
 
-        scaleSequence.AppendInterval(0.2f);
+        scaleSequence.AppendInterval(0.3f);
         // Step 1: Scale up coinButton
-        scaleSequence.Append(coinButton.transform.DOScale(new Vector3(1.3f, 1.3f, 1.3f), 0.3f));
+        scaleSequence.Append(coinButton.transform.DOScale(new Vector3(1.3f, 1.3f, 1.3f), 0.4f));
 
         // Add delay after Step 1
-        scaleSequence.AppendInterval(0.2f); // Adjust delay as needed
+        scaleSequence.AppendInterval(0.3f); // Adjust delay as needed
 
         // Step 2: Scale down coinButton back to original
-        scaleSequence.Append(coinButton.transform.DOScale(new Vector3(1f, 1f, 1f), 0.3f));
+        scaleSequence.Append(coinButton.transform.DOScale(new Vector3(1f, 1f, 1f), 0.4f));
 
         // Add delay after Step 2
-        scaleSequence.AppendInterval(0.2f);
+        scaleSequence.AppendInterval(0.3f);
 
         // Step 3: Scale up claimButton slightly
-        scaleSequence.Append(claimButton.transform.DOScale(new Vector3(0.56f, 0.56f, 0.56f), 0.15f));
+        scaleSequence.Append(claimButton.transform.DOScale(new Vector3(0.56f, 0.56f, 0.56f), 0.25f));
 
         // Add delay after Step 3
-        scaleSequence.AppendInterval(0.1f);
+        scaleSequence.AppendInterval(0.2f);
 
         // Step 4: Scale down claimButton back to original
-        scaleSequence.Append(claimButton.transform.DOScale(new Vector3(0.52f, 0.52f, 0.52f), 0.3f));
+        scaleSequence.Append(claimButton.transform.DOScale(new Vector3(0.52f, 0.52f, 0.52f), 0.4f));
 
         // Play the sequence
         scaleSequence.Play();
